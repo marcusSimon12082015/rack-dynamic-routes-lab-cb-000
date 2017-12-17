ead1f9c5a25306929e1b76a86d9d1664d3f4ca75
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-17 16:33:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,9 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-
+      item_name = req.path.split(/items/).last
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.price
     else
       resp.write "Route not found"
       resp.status = 404
