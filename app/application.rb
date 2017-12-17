0c8de7c0014c9b2438e18836fdf3a2da88b92f0a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-17 16:35:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split(/items/).last
+      item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       binding.pry
       resp.write item.price
